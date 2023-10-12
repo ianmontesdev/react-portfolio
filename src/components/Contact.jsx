1e499ede7ab2957e882e4e340c671223bcdfc7bc
Ian Montes <fotoianmontes@gmail.com>
2023-10-12 18:47:03 +0100

    installed dotenv dependency

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,44 @@
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+import "../assets/css/Contact.css";
+
 const Contact = () => {
-  return <h1>Test Contact</h1>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <main id="contact-container">
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="contact_name" placeholder="Your name" />
+        <input
+          type="email"
+          name="contact_email"
+          placeholder="Your contact email"
+        />
+        <textarea name="message" placeholder="Write your message..." />
+        <input type="submit" value="Send" />
+      </form>
+    </main>
+  );
 };
 
 export default Contact;
