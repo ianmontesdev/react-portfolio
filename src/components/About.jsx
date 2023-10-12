e6055c15bff1e23746fe11764876b3bf0e51e982
Ian Montes <fotoianmontes@gmail.com>
2023-10-12 23:30:30 +0100

    About component made responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,46 @@
+import "../assets/css/About.css";
+import amigariaImage from "../assets/img/amigaria_actividad_chat.gif";
+import imgProfile from "../assets/img/profile-design.jpg";
+
 const About = () => {
   return (
     <>
-      <h1>Test About</h1>
+      <main id="about-container">
+        <aside>
+          <section>
+            <img src={imgProfile} alt="" />
+            <div>
+              <p>Ian</p>
+              <p>Montes</p>
+            </div>
+            <div className="divisor"></div>
+            <div className="text-small">
+              <p>Full-Stack Developer</p>
+            </div>
+          </section>
+          <footer>
+            <i className="fa-brands fa-instagram"></i>
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-linkedin"></i>
+          </footer>
+        </aside>
+        <section>
+          <header>
+            <span>{"<Hello"}</span>
+            <span> {"World  />"}</span>
+          </header>
+          <article>
+            <pre>
+              {`
+            Junior web developer.
+            
+            I have been a graphic designer my entire career, but then discovered my passion about coding in 2022 while undergoing a VUE3 bootcamp, since then joined Higher Vocational Training on Web Apps Development (CFGS DAW).
+            
+            Currently studying 2nd year and, at the same time, studying 2nd year of Multiplatform Apps Development (CFGS DAM).`}
+            </pre>
+          </article>
+        </section>
+      </main>
     </>
   );
 };
