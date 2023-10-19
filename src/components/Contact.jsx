370646a1c796b55fbccd2d572ee5ec828c9eb4e7
Ian Montes <fotoianmontes@gmail.com>
2023-10-19 13:14:24 +0100

    added form validation and waiting status for emailjs response

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,55 @@
 import emailjs from "@emailjs/browser";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../assets/css/Contact.css";
 
 const Contact = () => {
   const form = useRef();
 
-  const [sendState, setSendState] = useState("");
-  const [sendError, setSendError] = useState("");
+  const [sendState, setSendState] = useState(0);
+  const [sendMsg, setSendMsg] = useState("");
+
+  const [nameValid, setNameValid] = useState(false);
+  const [emailValid, setEmailValid] = useState(false);
+  const [messageValid, setMessageValid] = useState(false);
+
+  useEffect(() => {
+    switch (sendState) {
+      case 0:
+        setSendMsg(<></>);
+        break;
+      case 1:
+        setSendMsg(
+          <>
+            <div className="lds-ring">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </>
+        );
+        break;
+      case 2:
+        setSendMsg(
+          <>
+            <p>Great! Your message was sent</p>
+          </>
+        );
+        break;
+      case 3:
+        setSendMsg(
+          <>
+            <p>Something went wrong, please try again</p>
+          </>
+        );
+        break;
+    }
+  }, [sendState]);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
+    setSendState(1);
     emailjs
       .sendForm(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
@@ -21,20 +60,50 @@ const Contact = () => {
       .then(
         (result) => {
           if (result.text == "OK") {
-            setSendError("");
-            setSendState("Great! Your message was sent");
+            setSendState(2);
           } else {
-            setSendState("");
-            setSendError("Something went wrong, please try again");
+            setSendState(3);
           }
         },
         (error) => {
-          setSendState("");
-          setSendError("Something went wrong, please try again");
+          setSendState(3);
           console.log(error.text);
         }
       );
   };
+
+  let invalidFeedback = "fa-solid fa-triangle-exclamation";
+  let validFeedback = "fa-solid fa-check feedback-valid";
+
+  function validateName() {
+    form.current.contact_name.value.length !== 0
+      ? setNameValid(true)
+      : setNameValid(false);
+  }
+
+  function validateEmail() {
+    form.current.contact_email.value.length !== 0 &&
+    /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(form.current.contact_email.value)
+      ? setEmailValid(true)
+      : setEmailValid(false);
+  }
+
+  function validateMessage() {
+    form.current.message.value.length !== 0
+      ? setMessageValid(true)
+      : setMessageValid(false);
+  }
+
+  function validateData(e) {
+    validateName();
+    validateEmail();
+    validateMessage();
+    if (nameValid && emailValid && messageValid) {
+      sendEmail(e);
+    } else {
+      e.preventDefault();
+    }
+  }
 
   function toggleLabel(event) {
     let label = event.target.nextElementSibling;
@@ -47,24 +116,51 @@ const Contact = () => {
 
   return (
     <main id="contact-container">
-      <form ref={form} onSubmit={sendEmail}>
+      <form
+        ref={form}
+        onSubmit={(e) => {
+          validateData(e);
+        }}
+      >
         <fieldset>
-          <input type="text" name="contact_name" onBlur={toggleLabel} />
+          <input
+            type="text"
+            name="contact_name"
+            onBlur={(e) => {
+              toggleLabel(e);
+              validateName();
+            }}
+          />
           <label htmlFor="contact_name">Name:</label>
+          <i className={nameValid ? validFeedback : invalidFeedback}></i>
         </fieldset>
         <fieldset>
-          <input type="email" name="contact_email" onBlur={toggleLabel} />
+          <input
+            type="email"
+            name="contact_email"
+            onBlur={(e) => {
+              toggleLabel(e);
+              validateEmail();
+            }}
+          />
           <label htmlFor="contact_email">E-mail:</label>
+          <i className={emailValid ? validFeedback : invalidFeedback}></i>
         </fieldset>
         <fieldset>
-          <textarea name="message" onBlur={toggleLabel} />
+          <textarea
+            name="message"
+            onBlur={(e) => {
+              toggleLabel(e);
+              validateMessage();
+            }}
+          />
           <label htmlFor="message">Message:</label>
+          <i className={messageValid ? validFeedback : invalidFeedback}></i>
         </fieldset>
-        <input type="submit" value="Send" />
-        <p>
-          {sendState}
-          {sendError}
-        </p>
+        <fieldset id="sendBtn">
+          <input type="submit" value="Send" />
+          {sendMsg}
+        </fieldset>
       </form>
       <section id="contact-info">
         <ul>
